69f991a0018170c787f0512c7d6bd6456a34ac55
Andrew Stanton <up683693@myport.ac.uk>
2020-10-13 18:49:30 +0100

    Search dirs to open in ranger

--- scripts/s.sh
+++ scripts/s.sh
@@ -3,3 +3,7 @@
 s(){
 du -a ~/.config ~/Dropbox/ ~/Documents ~/dr2 | awk '{print $2}' | fzf | xargs -r $EDITOR;
 }
+
+f(){
+du ~/.config ~/Dropbox/ ~/Documents ~/dr2 | awk '{print $2}' | fzf | xargs -r urxvtcd -e ranger;
+}
