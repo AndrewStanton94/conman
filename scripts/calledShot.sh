ff7cbcca5bf76a65a010d7e3344b01dbf4af6293
Andrew Stanton <up683693@myport.ac.uk>
2020-10-19 13:58:56 +0100

    Pause and formatting
    - Added a 0.5s sleep to prevent taking a screenshot of the name prompt
    - Put each command on a new line

--- scripts/calledShot.sh
+++ scripts/calledShot.sh
@@ -7,4 +7,7 @@ echo "${filename}"
 name="${screenshotsDir}${filename}"
 echo "${name}"
 
-[ ! -f $name ] && scrot "${filename}" --focused -e 'mv $f ~/Pictures/scrot/' && notify-send --icon /usr/share/icons/breeze-dark/devices/64/video-display.svg 'scrot' "Window screenshot called ${name}"
+[ ! -f $name ] \
+	&& sleep 0.5s \
+	&& scrot "${filename}" --focused -e 'mv $f ~/Pictures/scrot/' \
+	&& notify-send --icon /usr/share/icons/breeze-dark/devices/64/video-display.svg 'scrot' "Window screenshot called ${name}"
