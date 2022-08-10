6b70ab8d713137014410bd077ad1e9d52f1de6e8
Andrew Stanton <up683693@myport.ac.uk>
2022-08-10 18:53:05 +0100

    Awesome config

--- programConfigs/awesome/rc.lua
+++ programConfigs/awesome/rc.lua
@@ -45,10 +45,12 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init("/home/andrew/.config/awesome/nightVale/theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "gtk/theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "xresources/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
--- terminal = "urxvt"
 terminal = "alacritty"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
